f96af690819c45a738e76d56c3e051fde9734eec
Raul-Albuquerque <130876489+Raul-Albuquerque@users.noreply.github.com>
2023-07-07 18:19:11 -0300

    Add files via upload

---
+++ tipos_primitivos.js
@@ -0,0 +1,23 @@
+//STRING -> SEQUENCIA DE CARACTERES - entre aspas
+
+let string = "Está é uma string";
+
+//NUMBER -> NÚMEROS, POSITIVOS OU NEGATIVOS, INTEIROS OU DECIMAIS
+
+let number = 25;
+
+//BOOLEAN -> REPRESENTA VALORES VERDADEIROS OU FALSOS
+
+let boolean = true;
+
+//NULL -> NULO OU VAZIO
+
+let variavelNull = null;
+
+//UNDEFINED -> O VALOR NÃO FOI DEFINIDO
+
+let varUndefined = '';
+
+//typeof vai demonstrar de que tipo é a variável
+
+
---
+++ tipos_primitivos_exercicios.js
@@ -0,0 +1,32 @@
+//EXERCÍCIO 1
+
+//Declare uma variável com o nome:
+
+/*
+    tipoTexto e atribua a ela um valor do tipo string;
+    tipoNumero e atribua a ela um valor do tipo number;
+    verdadeiro e atribua a ela um valor do tipo boolean correspondente ao nome;
+    falso e atribua a ela um valor do tipo boolean correspondente ao nome;
+    tipoNulo com o valor null;
+    tipoIndefinido e sem valor.
+*/
+
+let tipoTexto = 'Texto';
+let tipoNumero = 12;
+let verdadeiro = true; 
+let falso = false;
+let tipoNulo = null;
+let tipoIndefinido
+
+//EXERCÍCIO 2
+
+/*
+    Corrija os valores:
+    let nome = true;
+    let idade = 'Harry Potter';
+    let souUmBruxo = 210;
+*/
+
+nome = 'Harry Potter';
+idade = 210;
+souUmBruxo = true;
