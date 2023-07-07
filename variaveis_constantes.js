b8841b083875d36621d28f7f3f22e541363b040c
Raul-Albuquerque <130876489+Raul-Albuquerque@users.noreply.github.com>
2023-07-07 12:51:57 -0300

    Add files via upload

---
+++ variaveis_constantes.js
@@ -0,0 +1,42 @@
+//Vamos ver como essa variável caixa e seu valor 'camiseta' são escritos em javascript
+
+let roupa = 'camiseta';
+
+//Atrubuindo um novo valor a variável
+
+roupa = 'moletom';
+
+let [nome] = [valor];
+
+//camelCase
+let comidaFavorita = 'Pizza';
+
+comidaFavorita = 'Lasanha';
+
+console.log(comidaFavorita)
+
+// Diferenças entre var, let e const
+// let e const são os atualmente utilizados, var é antigo e não deve ser utilizado
+//utilizando let
+let meuNome = 'Raul';
+
+meuNome = 'João';
+
+console.log(meuNome);
+
+//utilizando var
+var meuNome1 = 'Raul';
+
+meuNome1 = 'João';
+
+console.log(meuNome1);
+
+//const -> significa constante e seu valor não poderá ser alterado
+const meuNome2 = 'Raul';
+
+//será recebida uma mensagem de erro
+meuNome2 = 'João';
+
+console.log(meuNome2);
+
+
---
+++ variaveis_constantes_exercicios.js
@@ -0,0 +1,49 @@
+//EXERCÍCIO 1
+
+/*
+    Crie uma variável chamada cor, que pode ter seu valor alterado e atribua à ela o nome da cor 'verde'.
+    Lembre-se de colocar a cor entre aspas simples ('').
+*/
+
+let cor = 'verde';
+
+//EXERCÍCIO 2
+
+/*
+    Crie uma variável chamada cidade e atribua à ela o nome 'Parauapebas'. 
+    Depois, mude o nome da cidade armazenada na variável cidade para 'Rio de Janeiro'. 
+    Lembre-se de colocar o nome da cidade entre aspas simples ('').
+*/
+
+let cidade =  'Parauapebas';
+cidade = 'Rio de Janeiro';
+
+//EXERCÍCIO 3
+
+/*
+    Crie uma constante chamada meuNome e atribua à ela o seu nome entre aspas simples ('').
+*/
+
+const meuNome = 'Raul';
+
+//EXERCÍCIO 4
+
+/*
+    Crie uma variável chamada minhaIdade e atribua à ela o número que representa a sua idade.
+*/
+
+let minhaIdade = 26;
+
+//EXERCÍCIO 5
+
+/*
+    O seguinte código retorna um erro:
+    const email = email@trybe.com;
+    email = outroEmail@trybe.com;
+*/
+
+//CORREÇÃO
+/*
+    let email = 'email@trybe.com';
+    email = 'outroEmail@trybe.com';
+*/
