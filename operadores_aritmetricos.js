6b59a95ab175b5447e9a535866469cabe5419cdf
Raul-Albuquerque <130876489+Raul-Albuquerque@users.noreply.github.com>
2023-07-09 17:50:11 -0300

    Add files via upload

---
+++ operadores_aritmetricos.js
@@ -0,0 +1,43 @@
+//ordem das operações
+//PEMDAS
+
+//P arênteses
+//E xponenciação
+//MD multiplicação ou divisão
+//AS Adição ou Subtração
+
+//** exponenciação -> 2 ** 2; = 4
+// % módulo -> retorna o resto de uma divisão
+
+// EXERCÍCIOS
+
+/*
+    Crie a variável primeiroNumero e atribua a ela o valor 178;
+    Crie a variável segundoNumero e atribua a ela o valor 93;
+    Crie a variável resultadoDaSoma e atribua a ela a soma entre as variáveis primeiroNumero e segundoNumero.
+*/
+
+primeiroNumero = 178;
+segundoNumero = 93;
+resultadoDaSoma = primeiroNumero + segundoNumero;
+
+/*
+    Crie a variável primeiroNumero e atribua a ela o valor 133;
+    Crie a variável segundoNumero e atribua a ela o valor 87;
+    Crie a variável resultadoDaSubtracao e atribua a ela a subtração entre as variáveis primeiroNumero e segundoNumero.
+*/
+
+primeiroNumero = 133;
+segundoNumero = 87;
+resultadoDaSubtracao = primeiroNumero - segundoNumero;
+
+/*
+    Crie a variável dividendo e atribua a ela o valor 192;
+    Crie a variável divisor e atribua a ela o valor 12;
+    Crie a variável resultadoDaDivisao e atribua a ela a divisão da variável dividendo pela variável divisor.
+*/
+
+dividendo = 192;
+divisor = 12;
+resultadoDaDivisao = dividendo / divisor;
+
