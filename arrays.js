1463e9ebed4a7e09083baae74c465b406ba7efea
Raul Albuquerque <130876489+Raul-Albuquerque@users.noreply.github.com>
2023-07-17 00:42:11 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,37 @@
+// ARRAYS permitem que sejam armazenados mais de um valor separados por vírgula
+
+const pizzas = ['4 Queijos', 'frango com catupiry', 'alho', 'chocolate', 'pepperoni'];
+const pizzaEscolhida = pizzas[1];
+
+// adicionando valor a um array sabendo o valor do índice disponível
+const listraDeCompras = ['arroz', 'feijão', 'ovo'];
+listraDeCompras[3] = farofa;
+
+//adicionando valor a um array no final da lista
+listraDeCompras.push('Farofa');
+
+//substituindo valores de um array
+const estadosNorte = ['Acre', 'Amapá', 'Amazonas', 'Paraná', 'Rondônia', 'Roraima', 'Tocantins'];
+estadosNorte[3] = 'Pará';
+
+//contando a quantidade de itens de um array
+
+const convidados = ['Sheila', 'Cleyton', 'Leticia', 'Gisele', 'Vinicius', 'Ivan', 'Laura', 'Guilherme', 'Erickson', 'Claudio', 'Adilson', 'Juliana'];
+const totalConvidad0s = convidados.length;
+
+//Exercício 1
+
+/*
+    Imagine que você está saindo para fazer compras no supermercado com a seguinte lista:
+
+    Maçã
+    Biscoito/Bolacha
+    Banana
+    Café em pó
+    Leite
+    Arroz
+    Crie uma variável chamada listaDeCompras e atribua um array com as informações da lista acima.
+*/
+
+listaDeCompras = ['Maçã', 'Biscoito/Bolacha', 'Banana', 'Café em pó', 'Leite', 'Arroz'];
+
